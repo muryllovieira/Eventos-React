ea5f17d54baf1b2231629bc5b62d468380e0a54e
muryllovieira <muryllovieira59@gmail.com>
2025-03-07 09:42:57 -0300

    gerenciando eventos do react native

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { helloDarthVader } from './src/functions/functions';
 
 export default function App() {
 
   async function handlerHelloWorld() {
     console.log('hello world')
+
   }
 
   async function handlerPress(id: number) {
@@ -14,15 +15,34 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text onPress={() => console.log("inline")}>Inline</Text>
+
+      <TextInput
+        style={{ fontSize: 32 }}
+        placeholder='olá'
+        onChange={() => console.log("onCharge executado")}
+        onChangeText={() => console.log("onCharge Text acionado")}
+        onFocus={() => console.log("USUARIO FOCOU NESSE COMPONENTE")}
+        />
+
+      <Text
+        style={{ fontSize: 32 }}
+        onPress={() => console.log("1 pressionado")}
+        onPressIn={() => console.log("2 press in acionado")}
+        onPressOut={() => console.log("3 press out acionado")}
+        onLongPress={() => console.log("4 longpress acionando")}
+        onTextLayout={() => console.log("TEXTO NO LAYOUT")}
+      >
+        component principal
+      </Text>
+      {/* <Text onPress={() => console.log("inline")}>Inline</Text>
       <Text onPress={handlerHelloWorld}>Handler Function</Text>
       <Text>Handler Function With Parameters</Text>
       <Text onPress={() => handlerPress(1)}>
         Open up App.tsx to start working on your app!</Text>
 
       <Text onPress={helloDarthVader}>
-        Another File</Text>
-      <StatusBar style="auto" />
+        Another File</Text> */}
+
     </View>
   );
 }
@@ -33,5 +53,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
 });
